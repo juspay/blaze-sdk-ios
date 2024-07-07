4dc75765edffa6eff68f6f84174094436526d236
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-07-07 19:27:08 +0530

    BZ-27122: init: basic iOS SDK setup
    - initiased basic project with basic framework boilerplate

---
+++ zephyr/zephyr.h
@@ -0,0 +1,18 @@
+//
+//  zephyr.h
+//  zephyr
+//
+//  Created by Sahil Sinha on 07/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for zephyr.
+FOUNDATION_EXPORT double zephyrVersionNumber;
+
+//! Project version string for zephyr.
+FOUNDATION_EXPORT const unsigned char zephyrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zephyr/PublicHeader.h>
+
+
